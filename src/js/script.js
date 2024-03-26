a1216ba7f50dd2e92984120dcad0b6e179e5685c
valeeriiiee <obleavalerie@gmail.com>
2024-03-26 22:19:42 +0800

    installed notiflix, axios, and simplelightbox and edited js files

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+import { BASE_URL, options } from "./pixabay-ai.js";
+import axios from "axios";
+import { Notify } from "notiflix/build/notiflix-notify-aio.js";
+import simpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
